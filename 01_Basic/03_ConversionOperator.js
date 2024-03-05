22d94d80445c9d22f2daae44587cc7374cf66564
Shahanshah <shahanshah.967@gmail.com>
2024-03-05 20:34:08 +0530

    Opertor

--- 01_Basic/03_ConversionOperator.js
+++ 01_Basic/03_ConversionOperator.js
@@ -4,3 +4,16 @@ console.log(typeof score);
 let valueINNumber = Number(score);
 console.log(typeof valueINNumber);
 console.log(valueINNumber);
+
+// ******************************************OPERATION*********************
+
+let value = 3;
+let negValue = -value;
+
+console.log(negValue);
+
+let str1 = "hello";
+let str2 = "ansari";
+let str3 = str1 + str2;
+
+console.log(str3);
