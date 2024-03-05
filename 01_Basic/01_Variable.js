1a28d770380a6bb99734590f9725bb5d350819bc
Shahanshah <shahanshah.967@gmail.com>
2024-03-05 16:00:43 +0530

    Variables

---
+++ 01_Basic/01_Variable.js
@@ -0,0 +1,8 @@
+const accountId = 144553;
+let accountEmail = "ansari@gmail.com";
+var accountPassword = "12345";
+accountCity = "Gaya";
+
+// accountId = 2; // Not Allowed
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
