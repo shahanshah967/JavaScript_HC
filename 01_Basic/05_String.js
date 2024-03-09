78ff699f85a1ab47e9f45b90c4a894eb91060f47
Shahanshah <shahanshah.967@gmail.com>
2024-03-09 16:08:18 +0530

    Completed String

---
+++ 01_Basic/05_String.js
@@ -0,0 +1,20 @@
+const name = "hitesh";
+const repoCount = 50;
+
+console.log(name + repoCount);
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("Ansari-sa");
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+const newString = gameName.substring(0, 4);
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4);
+
+console.log(anotherString);
