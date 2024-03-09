72292740817781898f6df29fbceb67bed5a86cc0
Shahanshah <shahanshah.967@gmail.com>
2024-03-09 18:32:59 +0530

    Math And Num

---
+++ 01_Basic/06_NumsMath.js
@@ -0,0 +1,30 @@
+const score = 400;
+const balance = new Number(100);
+
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966;
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000;
+
+// console.log(hundreds.toLocaleString("en-IN"));
+
+// +++++++++++++++++++++++++++ MAth ++++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+
+console.log((Math.random() * 10) + 1);
+
+const min =1;
+const max = 6
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
