446ca3cfc9ecbf6eb4e1f7ffa65f8841ec253b5a
Shahanshah <shahanshah.967@gmail.com>
2024-03-15 15:22:23 +0530

    Object and JSON Talk

---
+++ 02_Basic/03_Object.js
@@ -0,0 +1,11 @@
+const JsUser = {
+    name:"Hitesh",
+    "fullname":"Hitesh Chaudhry",
+    age:18,
+    location:'Jaipur',
+    email:'hitesh@google.com'
+}
+
+console.log(JsUser.email)
+console.log(JsUser['email'])
+console.log(JsUser.fullname)
