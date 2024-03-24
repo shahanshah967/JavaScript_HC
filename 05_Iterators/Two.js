6489025e8ac8f1a7c0b11f0efd9a879771374339
Shahanshah <shahanshah.967@gmail.com>
2024-03-25 00:42:05 +0530

    While & Do-While Loop

---
+++ 05_Iterators/Two.js
@@ -0,0 +1,20 @@
+let index = 0;
+// while (index <= 10) {
+//   console.log(`Value of index is ${index}`);
+//   index++;
+// }
+
+let myArray = ["flash", "batman", "superman"];
+
+let arr = 0;
+
+while (arr < myArray.length) {
+  console.log(`Value at ${arr} is ${myArray[arr]}`);
+  arr++;
+}
+
+let score = 1;
+do {
+  console.log(`Score is ${score}`);
+  score++;
+} while (score <= 10);
